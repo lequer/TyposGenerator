f176ee5ccf0513c436659f6b197baebada65df55
Michel Le Quer <michel@mlequer.com>
2020-02-10 12:59:54 +0000

    Resolve "Add tests"

--- tests/Provider/MissedCharTyposProviderTest.php
+++ tests/Provider/MissedCharTyposProviderTest.php
@@ -1,5 +1,22 @@
 <?php
 
+/** @noinspection ALL */
+
+/******************************************************************************
+ * @author Michel Le Quer <michel@mlequer.com> - https://mlequer.com          *
+ * @version 1.0.0                                                             *
+ * @license see LICENSE included in package                                   *
+ *                                                                            *
+ ******************************************************************************/
+
+/******************************************************************************
+ * @author Michel Le Quer <michel@mlequer.com> - https://mlequer.com          *
+ * @version 1.0.0                                                             *
+ * @license see LICENSE included in package                                   *
+ *                                                                            *
+ ******************************************************************************/
+
+
 namespace MLequer\Component\Typos\Provider;
 
 use PHPUnit\Framework\TestCase;
@@ -7,11 +24,29 @@ use PHPUnit\Framework\TestCase;
 class MissedCharTyposProviderTest extends TestCase
 {
 
+
     /**
      * @covers \MLequer\Component\Typos\Provider\MissedCharTyposProvider
      */
     public function testGenerateTypos(): void
     {
-        $this->assertTrue(true);
+        $provider  = new MissedCharTyposProvider();
+        $generator = $provider->generateTypos('computer');
+        $res       = iterator_to_array($generator, false);
+        $this->assertEquals($this->expected, $res);
     }
+
+    /**
+     * @var array<string>
+     */
+    private array $expected = [
+        "omputer",
+        "cmputer",
+        "coputer",
+        "comuter",
+        "compter",
+        "compuer",
+        "computr",
+        "compute",
+    ];
 }
